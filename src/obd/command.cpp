dc5975aa5e8f9561581d42291a988898d1d71c9f
eyeball29 <104666786+eyeball29@users.noreply.github.com>
2024-03-08 11:37:52 -0500

    Update command.cpp
    added suggested cmds from issue#110 of dev branch as a note until other files in repo are ready. Will follow with additional file updates and a notes file on the process of updating the content or adding a new gauge display

--- src/obd/command.cpp
+++ src/obd/command.cpp
@@ -7,3 +7,8 @@ Commands cmds = {{"Calculated Engine Load", QCanBusFrame(0x7df, QByteArray::from
                  {"Vehicle Speed", QCanBusFrame(0x7df, QByteArray::fromHex("02010D0000000000")), speed},
                  {"Intake Air Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02010F0000000000")), temp},
                  {"Mass Air Flow (MAF) Rate", QCanBusFrame(0x7df, QByteArray::fromHex("0201100000000000")), flow}};
+/* add-ons to original. Need to update other files                {"Engine Oil Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02015C0000000000")), oiltemp}, 
+                 {"Fuel Tank Value", QCanBusFrame(0x7df, QByteArray::fromHex("02012F0000000000")), fueltank}, 
+                 {"Lambda 1", QCanBusFrame(0x7df, QByteArray::fromHex("0201240000000000")), lambdaValue}, 
+                 {"CAT TEMP 1", QCanBusFrame(0x7df, QByteArray::fromHex("02013C0000000000")), catTemp}, 
+                 {"BOOST Pressure", QCanBusFrame(0x7df, QByteArray::fromHex("02010B0000000000")), boostPress}}; */
