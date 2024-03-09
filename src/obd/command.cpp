78ab7d9f9b0d32094bfc617ad19025c1983b6eeb
eyeball29 <104666786+eyeball29@users.noreply.github.com>
2024-03-08 20:05:01 -0500

    Update command.cpp

--- src/obd/command.cpp
+++ src/obd/command.cpp
@@ -1,14 +1,18 @@
 #include "obd/command.hpp"
 #include "obd/decoders.hpp"
 
-Commands cmds = {{"Calculated Engine Load", QCanBusFrame(0x7df, QByteArray::fromHex("0201040000000000")), percentage},
-                 {"Engine Coolant Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("0201050000000000")), temp},
-                 {"Engine Revolutions Per Minute (RPM)", QCanBusFrame(0x7df, QByteArray::fromHex("02010C0000000000")), rpm},
-                 {"Vehicle Speed", QCanBusFrame(0x7df, QByteArray::fromHex("02010D0000000000")), speed},
-                 {"Intake Air Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02010F0000000000")), temp},
-                 {"Mass Air Flow (MAF) Rate", QCanBusFrame(0x7df, QByteArray::fromHex("0201100000000000")), flow}};
-/* add-ons to original. Need to update other files                {"Engine Oil Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02015C0000000000")), oiltemp}, 
-                 {"Fuel Tank Value", QCanBusFrame(0x7df, QByteArray::fromHex("02012F0000000000")), fueltank}, 
-                 {"Lambda 1", QCanBusFrame(0x7df, QByteArray::fromHex("0201240000000000")), lambdaValue}, 
-                 {"CAT TEMP 1", QCanBusFrame(0x7df, QByteArray::fromHex("02013C0000000000")), catTemp}, 
-                 {"BOOST Pressure", QCanBusFrame(0x7df, QByteArray::fromHex("02010B0000000000")), boostPress}}; */
+Commands cmds = {
+                {"Calculated Engine Load", QCanBusFrame(0x7df, QByteArray::fromHex("0201040000000000")), percentage},
+                {"Engine Coolant Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("0201050000000000")), temp},
+                {"Engine Revolutions Per Minute (RPM)", QCanBusFrame(0x7df, QByteArray::fromHex("02010C0000000000")), rpm},
+                {"Vehicle Speed", QCanBusFrame(0x7df, QByteArray::fromHex("02010D0000000000")), speed},
+                {"Intake Air Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02010F0000000000")), temp},
+                {"Mass Air Flow (MAF) Rate", QCanBusFrame(0x7df, QByteArray::fromHex("0201100000000000")), flow}};
+/*add-ons to original. Need to update other files                {"Engine Oil Temperature", QCanBusFrame(0x7df, QByteArray::fromHex("02015C0000000000")), temp}, 
+                {"Fuel Tank Value", QCanBusFrame(0x7df, QByteArray::fromHex("02012F0000000000")), percentage}, 
+                {"Throttle", QCanBusFrame(0x7df, QByteArray::fromHex("0201110000000000")), percentage},
+                {"BOOST", QCanBusFrame(0x7df, QByteArray::fromHex("02010B3300000000")), pressure},
+                {"Lambda 1", QCanBusFrame(0x7df, QByteArray::fromHex("0201240000000000")), lambdaValue}, 
+                
+                {"CAT TEMP 1", QCanBusFrame(0x7df, QByteArray::fromHex("02013C0000000000")), temp} 
+                }; */
